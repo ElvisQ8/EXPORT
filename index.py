e0c6d7c7892093cedfe165436260f130a59b28ac
ElvisQ8 <civerelvis@gmail.com>
2025-03-10 15:10:15 -0500

    Update index.py

--- index.py
+++ index.py
@@ -35,7 +35,7 @@ def copy_data_to_template(df, sheet_name, selected_name):
             df_filtered.to_excel(writer, sheet_name=sheet_name, index=False)
         
         # Retornar el archivo Excel en formato BytesIO para ser descargado
-        output.seek(0)
+        output.seek(0)  # Posicionar el flujo de vuelta al inicio
         return output
 
 # Crear la interfaz de usuario
